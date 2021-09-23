949445744de7c7a97e1fdd1396370ecfbeb681e5
Luciano <regueraluciano@gmail.com>
2021-09-23 16:38:31 -0300

    [ADD] test added

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,23 @@
+module.exports = {
+    user: {
+      _id: "507f191e810c19729de860ea",
+      name: "Luciano",
+      username: "Luciano03",
+      password: "mystrongPassword"
+    },
+    users: [
+      {
+        _id: "507f191e810c19729de860ea",
+        name: "Luciano",
+        username: "Luciano03",
+        password: "mystrongPassword"
+      },
+      {
+        _id: "507f191e810c19729de860eb",
+        name: "Ignacio",
+        username: "Ignacio_34",
+        password: "mystrongPassword"
+      }
+    ]
+  };
+  
